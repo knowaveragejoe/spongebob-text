a41bc97ce27190937bf224bb605b80ab6d73ec21
Joseph Clark <25515709+knowaveragejoe@users.noreply.github.com>
2020-08-29 14:46:50 -0400

    Initial commit + working spongebob text component

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1>Spongebob Mocking Text Generator</h1>
+
+    <input type="text" v-model="inputText" />
+    <hr />
+    <h3>{{ spongebobText }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SpongebobTextGenerator",
+  computed: {
+    spongebobText() {
+      return this.inputText
+        .split("")
+        .map((char) => {
+          return Math.random() * 100 >= 50
+            ? char.toUpperCase()
+            : char.toLowerCase();
+        })
+        .join("");
+    },
+  },
+  data() {
+    return {
+      inputText: "",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
